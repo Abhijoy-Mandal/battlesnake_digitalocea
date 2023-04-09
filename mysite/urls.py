1c745a00d66eac1677697c98d371d25c216a945b
Abhijoy-Mandal <56208545+Abhijoy-Mandal@users.noreply.github.com>
2023-04-09 16:44:37 -0400

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import main
 
 urlpatterns = [
+    path("info/", main.info, name="info"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("", include("frontpage.urls")),
