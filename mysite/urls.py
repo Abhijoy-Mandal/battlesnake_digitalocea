0059df3ca81cf065ab7d929cbfc09c64b87e3ece
Abhijoy-Mandal <56208545+Abhijoy-Mandal@users.noreply.github.com>
2023-04-09 16:48:23 -0400

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-import main
+from . import main
 
 urlpatterns = [
     path("info/", main.info, name="info"),
